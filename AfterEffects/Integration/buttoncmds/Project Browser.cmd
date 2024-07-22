bcaeadbe8f668765ea1bfa80339eef25592e68ac
Animatect <animatect@gmail.com>
2024-07-21 21:00:48 -0600

    Correct installation

---
+++ AfterEffects/Integration/buttoncmds/Project Browser.cmd
@@ -0,0 +1,7 @@
+echo on
+REM CD /D "%~dp0"
+REM CD PRISMROOT
+REM start "PRISMPYTHONFOLDER/python.exe" "SCRIPTSFOLDER/Prism_AfterEffects_MenuTools.py" ProjectBrowser
+"PRISMPYTHONFOLDER/python.exe" "SCRIPTSFOLDER/Prism_AfterEffects_MenuTools.py" ProjectBrowser
+REM start PRISMPYTHONFOLDER\python.exe SCRIPTSFOLDER\Prism_AfterEffects_MenuTools.py ProjectBrowser
+REM pause
